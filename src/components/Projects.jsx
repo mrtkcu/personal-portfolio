5a8c05ba7406204356ffabd0466f80625096efd0
Murat Okçu <mrtkcu0101@gmail.com>
2025-01-03 00:40:51 +0300

    almost finished

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,124 +1,85 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "../context/LanguageContext";
+
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const { language, translations } = useContext(LanguageContext);
+
+  useEffect(() => {
+    axios
+      .get("/data/projectsData.json")
+      .then((response) => {
+        setProjects(response.data);
+      })
+      .catch((error) => {
+        console.log("Failed to fetch project data:", error);
+      });
+  }, []);
+
   return (
     <section className="dark:bg-gray-800">
       <h4 className="text-4xl leading-snug pt-5 pb-10 font-medium text-center">
         Projects
       </h4>
       <div className="flex flex-row gap-20 max-h-[100%] w-[50%] mx-auto overflow-y-visible p-3 z-0 xl-sm:w-[70%] xl-sm:gap-5 lg-sm2:w-[100%] lg-sm2:gap-5 sm2:w-[100%] sm2:gap-5 xs:p-0">
-        <div className=" flex flex-col justify-between bg-blue3 dark:bg-projectdark w-[49%] rounded-lg sm2:justify-start sm2:max-h-[500px] xs:max-h-[325px]">
-          <div className="p-10 flex flex-col gap-5 font-playfair xs:p-2 xs:gap-3 dark:text-gray-800">
-            <h5 className="font-playfair text-3xl font-semibold sm2:text-xl xs:text-base">
-              Project Portfolio
-            </h5>
-            <div>
-              <p className="mb-3 sm2:text-sm xs:text-xs">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-                aut, odit laborum aliquam voluptatum nisi mollitia
-              </p>
-              <div className="flex flex-wrap gap-2 w-[60%] sm2:w-[100%] sm2:text-sm xs:text-xs xs:gap-1">
-                <span className="bg-white  rounded-full px-3 py-1 sm2:px-2 sm2:py-0 hover:bg-white4 hover:cursor-pointer">
-                  react
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  vercel
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  axios
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  router
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0 hover:bg-white4 hover:cursor-pointer">
-                  router
-                </span>
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className=" flex flex-col justify-between bg-blue3 dark:bg-projectdark w-[49%] rounded-lg sm2:justify-start sm2:max-h-[500px] xs:max-h-[325px]"
+          >
+            <div className="p-10 flex flex-col gap-5 font-playfair xs:p-2 xs:gap-3 dark:text-gray-800">
+              <h5 className="font-playfair text-3xl font-semibold sm2:text-xl xs:text-base">
+                {project.title}
+              </h5>
+              <div>
+                <p className="mb-3 sm2:text-sm xs:text-xs">
+                  {language === "en"
+                    ? project["description-en"]
+                    : project["description-tr"]}
+                </p>
+
+                <div className="flex flex-wrap gap-2 w-[60%] sm2:w-[100%] sm2:text-sm xs:text-xs xs:gap-1">
+                  {project.tags.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="bg-white  rounded-full px-3 py-1 sm2:px-2 sm2:py-0 hover:bg-white4 hover:cursor-pointer"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="flex justify-between -mb-5 ">
+                <a
+                  className="hover:underline hover:text-white font-bold text-lg sm2:text-sm xs:text-xs"
+                  href={project.githubLink}
+                >
+                  {translations.githubText}
+                </a>
+                <a
+                  className="hover:underline hover:text-white font-bold text-lg sm2:block sm2:text-sm xs:text-xs"
+                  href={project.appLink}
+                >
+                  {translations.appText} &rarr;
+                </a>
               </div>
             </div>
-            <div className="flex justify-between -mb-5 ">
-              <a
-                className="hover:underline hover:text-white font-bold text-lg sm2:text-sm xs:text-xs"
-                href=""
-              >
-                View on Github
-              </a>
-              <a
-                className="hover:underline hover:text-white font-bold text-lg sm2:block sm2:text-sm xs:text-xs"
-                href=""
-              >
-                Go to app &rarr;
-              </a>
-            </div>
-          </div>
-          <div className="relative flex justify-center items-center h-[300px] rounded-lg translate-y-[20%] sm2:mt-[10%] xs:translate-y-[1.2rem]">
-            <img
-              className="absolute w-[70%] border-black"
-              src="https://www.figma.com/file/YuAwEInBB8GqOO7wNosr5j/image/4925c0fe0dd6fa4b81a4386c44fc6cd6fffb11d4"
-              alt=""
-            />
+            <div className="relative flex justify-center items-center h-[300px] rounded-lg translate-y-[20%] sm2:mt-[10%] xs:translate-y-[1.2rem]">
+              <img
+                className="absolute w-[71%]"
+                src={project.images[0]}
+                alt=""
+              />
 
-            <img
-              className="absolute right-[.05px]"
-              src="https://s3-alpha-sig.figma.com/img/600f/f411/f246155782db0594c00c688cdbf27f80?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VtQPvhuQm9yF8PI4DwiVX55RSTSl37IMTvFf9YCupFcWLQ5vl0tzf0~2gsiaZKspdF1OChOhYbG2MUuv8aJhz80wjfmM6ejTsuiGwZNiqIQcGtr0HiGBwVVx2uRjuhGCXejNgSu1B0uyWn~foSYmy-Gescy7RqH5hSeTwX3vj6-2zRMzqT9KDcOOsIUcOJGxxWoQbQsnefZTyLhOtKSzL9ZZ8PIVAMzp13NOtPGhtqwsB~p4~i8XkUqnvNZLWP5helrY1Ykk1JlAf~PoKCqkbSrCeTweH3va-d92LRDZd4hlE7EUiTLyDrGJ4plo-d9VsZwVoni2iscrKlq3B70dSA__"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className=" flex flex-col justify-between bg-blue3 dark:bg-projectdark w-[49%] rounded-lg sm2:justify-start sm2:max-h-[500px] xs:max-h-[325px]">
-          <div className="p-10 flex flex-col gap-5 font-playfair xs:p-2 xs:gap-3 dark:text-gray-800">
-            <h5 className="font-playfair text-3xl font-semibold sm2:text-xl xs:text-base">
-              Project Portfolio
-            </h5>
-            <div>
-              <p className="mb-3 sm2:text-sm xs:text-xs">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-                aut, odit laborum aliquam voluptatum nisi mollitia
-              </p>
-              <div className="flex flex-wrap gap-2 w-[60%] sm2:w-[100%] sm2:text-sm xs:text-xs xs:gap-1">
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0 hover:bg-white4 hover:cursor-pointer">
-                  react
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  vercel
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  axios
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0  hover:bg-white4 hover:cursor-pointer">
-                  router
-                </span>
-                <span className="bg-white rounded-full px-3 py-1 sm2:px-2 sm2:py-0 hover:bg-white4 hover:cursor-pointer">
-                  router
-                </span>
-              </div>
-            </div>
-            <div className="flex justify-between -mb-5 ">
-              <a
-                className="hover:underline hover:text-white font-bold text-lg sm2:text-sm xs:text-xs"
-                href=""
-              >
-                View on Github
-              </a>
-              <a
-                className="hover:underline hover:text-white font-bold text-lg sm2:block sm2:text-sm xs:text-xs"
-                href=""
-              >
-                Go to app &rarr;
-              </a>
+              <img
+                className="absolute right-[.05px]"
+                src={project.images[1]}
+                alt=""
+              />
             </div>
           </div>
-          <div className="relative flex justify-center items-center h-[300px] rounded-lg translate-y-[20%] sm2:mt-[10%] xs:translate-y-[1.2rem]">
-            <img
-              className="absolute w-[70%] border-black"
-              src="https://s3-alpha-sig.figma.com/img/36bd/8a30/ecc54a92ea4c67cfd6f14ce412bbc06b?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=I2knOg~NIPq2oSvHa042ook4mzE5nDDioPOurNpcyfE3HJlPv3ZwihPfzen-~Ww4UPBbSbZOU-8v66-IBFV9XVSOimDxxVENTUNPkv2Vs-Qt~LgrRG~MxARtCD85L2yEU3Dzv-Kg1Oa-ESO8dejSmKnuozMnCdWaazsI7j1~aZ7llcW0mKOMjSIBcDV~fpBIWf7OM1c4fYhsmxOAu90ZRphKO3Q0aeZEJkxdh7LzmxO0xIm5ahf2rlF4cFPZXcHUmGHlxjOfw5aEa0K5cL8d5xZhpoGZZioW1vHi5HZbaqDUFoTTNKwDgXXk24Hx3bbBrIdbexy1-LFMBZnhdux7Ug__"
-              alt=""
-            />
-
-            <img
-              className="absolute"
-              src="https://s3-alpha-sig.figma.com/img/600f/f411/f246155782db0594c00c688cdbf27f80?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VtQPvhuQm9yF8PI4DwiVX55RSTSl37IMTvFf9YCupFcWLQ5vl0tzf0~2gsiaZKspdF1OChOhYbG2MUuv8aJhz80wjfmM6ejTsuiGwZNiqIQcGtr0HiGBwVVx2uRjuhGCXejNgSu1B0uyWn~foSYmy-Gescy7RqH5hSeTwX3vj6-2zRMzqT9KDcOOsIUcOJGxxWoQbQsnefZTyLhOtKSzL9ZZ8PIVAMzp13NOtPGhtqwsB~p4~i8XkUqnvNZLWP5helrY1Ykk1JlAf~PoKCqkbSrCeTweH3va-d92LRDZd4hlE7EUiTLyDrGJ4plo-d9VsZwVoni2iscrKlq3B70dSA__"
-              alt=""
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
